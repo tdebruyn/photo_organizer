c28b1f8c9133680369d63dc2f3be11815b8f0a15
Tom Debruyne <5540603+tdebruyn@users.noreply.github.com>
2020-01-11 12:46:50 +0100

    Add directory rename support features

--- photo_organizer/main.py
+++ photo_organizer/main.py
@@ -1,6 +1,6 @@
-from PySide2.QtCore import QThread, QObject, Signal, Slot, Qt, QSize, QUrl
-from PySide2.QtWidgets import (QApplication, QWidget, QPushButton,
-                             QRadioButton, QVBoxLayout, QLineEdit, QSlider,
+from PySide2.QtCore import QThread, QObject, Signal, Slot, Qt, QSize, QUrl, QStringListModel
+from PySide2.QtWidgets import (QApplication, QWidget, QPushButton, QCompleter,
+                             QRadioButton, QVBoxLayout, QLineEdit, QSlider, QListWidget,
                              QLabel, QHBoxLayout, QFileDialog, QGroupBox, QSizePolicy)
 from PySide2 import QtCore
 from send2trash import send2trash
@@ -10,6 +10,7 @@ from pathlib import Path
 from dataclasses import dataclass
 from pkg_resources import resource_filename
 from datetime import datetime
+from collections import deque
 deleted_picture = resource_filename(__name__, 'bitmaps/deleted-picture.png')
 
 
@@ -21,6 +22,29 @@ class image(object):
     size: QSize = QSize(0, 0)
     to_delete: bool = False
     deleted: bool = False
+
+class listview():
+    def __init__(self, items):
+        if not isinstance(items, list):
+            raise TypeError(f"listview constructor expects a list but received a {type(items)} : {items}")
+        self.items = deque(set(items))
+
+    def add_item(self, item):
+        if item not in self.items:
+            self.items.append(item)
+        else:
+            self.items.remove(item)
+            self.items.append(item)
+
+    def remove_item(self, item):
+        if item not in self.items:
+            raise KeyError(f"No such item: {item}")
+        else:
+            self.items.remove(item)
+            self.items.appendleft(item)
+
+    def top_items(self, count):
+        return list(self.items)[-count:]
 
 class CacheMgr(QObject):
     def __init__(self, image_names):
@@ -77,6 +101,7 @@ class AppWindow(QWidget):
         super().__init__()
         self.images = []
         self.current_image = 0
+        self.rel_dir = set()
 
         self.build_UI()
         self.init_crawling()
@@ -94,29 +119,25 @@ class AppWindow(QWidget):
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
         self.setStyleSheet("QWidget{background-color: rgb(100,100,100)}")
 
-        # app title
-        title = QLabel(self)
-        title.setStyleSheet("color: rgb(170, 200, 255);"
-                            "font: 40px;")
-        title.setText("Photos Organizer")
-        title.openExternalLinks()
-
         # current file name
         self.fnlbl = QLabel(self)
-        self.fnlbl.setStyleSheet("font: 20px;")
-
-        self.lbl2 = QLabel(self)
-        self.lbl2.setStyleSheet("color: rgb(170, 200, 255);"
-                               "font: 14px;")
+        self.fnlbl.setStyleSheet("font: 16px;")
 
         # new name text box
         self.new_name = QLineEdit()
-        self.new_name.setStyleSheet("font: 20px;")
+        self.new_name.setStyleSheet("color: rgb(252, 90, 90);"
+            "font: 16px;")
         self.new_name.setPlaceholderText("Enter new name, min 3 chars, \"del\" to delete picture")
         self.new_name.returnPressed.connect(self.rename_pict)
 
+        self.completer = QCompleter(self.rel_dir, self)
+        self.completer.setCaseSensitivity(Qt.CaseInsensitive)
+        self.completer.setFilterMode(Qt.MatchContains)
+
         self.new_dir = QLineEdit()
-        self.new_dir.setStyleSheet("font: 20px;")
+        self.new_dir.setCompleter(self.completer)
+        self.new_dir.setStyleSheet("color: rgb(182, 255, 64);"
+                                   "font: 16px;")
         self.new_dir.setPlaceholderText("Enter new directory name")
         self.new_dir.returnPressed.connect(self.rename_pict)
 
@@ -172,31 +193,33 @@ class AppWindow(QWidget):
                               "color: rgb(150,150,150)")
         commit_btn.clicked.connect(self.commit_changes)
 
+        stopbtn = QPushButton("Stop", self)
+        stopbtn.setFlat(True)
+        stopbtn.setStyleSheet("border-style: outset;"
+                              "background-color: rgb(60,60,60);"
+                              "color: rgb(150,150,150)")
+        stopbtn.clicked.connect(self.closeall)
+
         r_fn = QRadioButton("Filename")
         r_fn.setChecked(True)
         r_fn.toggled.connect(self.new_name.setVisible)
-
         r_fn.toggled.connect(self.new_name.setFocus)
         r_fn.toggled.connect(self.new_dir.setHidden)
 
-
         r_dir = QRadioButton("Directory")
         r_dir.toggled.connect(self.new_name.setHidden)
-
         r_dir.toggled.connect(self.new_dir.setVisible)
         r_dir.toggled.connect(self.new_dir.setFocus)
-
 
         r_fn_dir = QRadioButton("Filename and Directory")
         r_fn_dir.toggled.connect(self.new_name.setVisible)
         r_fn_dir.toggled.connect(self.new_dir.setVisible)
 
-
         # set up the label widget to display the pic
-        self.picture_height = 600
         self.picture = QLabel(self)
         self.picture.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
 
+        info_box = QVBoxLayout()
         group_box = QGroupBox()
         self.pic_id = QLabel()
         self.pic_size = QLabel()
@@ -205,39 +228,46 @@ class AppWindow(QWidget):
         group_box_layout.addWidget(self.pic_id)
         group_box_layout.addWidget(self.pic_size)
         group_box_layout.addWidget(self.pic_action)
-
         group_box.setLayout(group_box_layout)
         group_box.setFixedWidth(200)
-        group_box.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
 
+        last_dirs_tooltip = """If \"Directory\" is selected as update type:
+            Single-click to set selected directory
+            Double-click to set selected directory and move to next image
+            Right-click to remove directory from list"""
+        self.last_dirs_view = QListWidget()
+        self.last_dirs_view.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.last_dirs_view.itemClicked.connect(self.update_dir_field)
+        self.last_dirs_view.itemDoubleClicked.connect(self.update_dir_next)
+        self.last_dirs_view.setToolTip(last_dirs_tooltip)
 
-        stopbtn = QPushButton("Stop", self)
-        stopbtn.setFlat(True)
-        stopbtn.setStyleSheet("border-style: outset;"
-                              "background-color: rgb(60,60,60);"
-                              "color: rgb(150,150,150)")
-        stopbtn.clicked.connect(self.closeall)
+        info_box.addWidget(group_box)
+        info_box.addStretch(1)
+        info_box.addWidget(QLabel("Last used directories"))
+        info_box.addWidget(self.last_dirs_view)
 
         vbox = QVBoxLayout()
         h_buttons = QHBoxLayout()
-        v_radio = QVBoxLayout()
-        v_line_edit = QVBoxLayout()
         h_work = QHBoxLayout()
 
+        v_radio_box = QGroupBox()
+        v_radio = QVBoxLayout()
+        v_radio_box.setTitle("Update type")
         v_radio.addWidget(r_fn)
         v_radio.addWidget(r_dir)
         v_radio.addWidget(r_fn_dir)
+        v_radio_box.setLayout(v_radio)
 
+        v_line_edit = QVBoxLayout()
         v_line_edit.addWidget(self.fnlbl)
-        v_line_edit.addWidget(self.lbl2)
         v_line_edit.addWidget(self.new_name)
         v_line_edit.addWidget(self.new_dir)
 
         h_work.addLayout(v_line_edit)
-        h_work.addLayout(v_radio)
+        h_work.addWidget(v_radio_box)
 
         group_picture = QHBoxLayout()
-        group_picture.addWidget(group_box, alignment=Qt.AlignTop)
+        group_picture.addLayout(info_box) #, alignment=Qt.AlignTop)
         group_picture.addWidget(self.picture)
 
         h_buttons.addWidget(prev_btn)
@@ -248,12 +278,23 @@ class AppWindow(QWidget):
         h_buttons.addWidget(open_btn)
         h_buttons.addWidget(stopbtn)
 
-        vbox.addWidget(title)
         vbox.addLayout(group_picture)
         vbox.addWidget(self.picture_slider)
         vbox.addLayout(h_work)
         vbox.addLayout(h_buttons)
         self.setLayout(vbox)
+
+    @Slot()
+    def update_dir_field(self):
+        if self.new_dir.isVisible():
+            self.new_dir.setText(self.last_dirs_view.currentItem().text())
+
+    @Slot()
+    def update_dir_next(self):
+        if self.new_dir.isVisible():
+            self.new_dir.setText(self.last_dirs_view.currentItem().text())
+            self.rename_pict()
+
 
     @Slot()
     def del_image(self):
@@ -277,13 +318,13 @@ class AppWindow(QWidget):
 
         self.next_image()
         self.new_name.clear()
+        self.rel_dir.add(self.new_dir.text())
+        self.completer.setModel(self.rel_dir)
 
 
     @Slot()
     def closeall(self):
-        self.lbl2.setText("STOPPING, please wait")
         self.dir_crawler.running = False
-        self.lbl2.repaint()
         QApplication.instance().processEvents()
         self.crawler_thread.exit()
         self.crawler_thread.wait()
@@ -338,11 +379,15 @@ class AppWindow(QWidget):
         self.current_image = position
         cur_img_name = self.images[self.current_image].path
         s_cur_img_name = str(cur_img_name)
-        if len(str(cur_img_name.parent)) > self.size().width()//30:
-            s_cur_img_name_parent = f"[...]{str(cur_img_name.parent)[-self.size().width()//30:]}"
+        relative_path = Path(cur_img_name.parent).relative_to(self.dir_crawler.root_dir)
+        if str(relative_path) == ".":
+            relative_path = ""
         else:
-            s_cur_img_name_parent = cur_img_name.parent
-        text_to_set = f"<font color=\"#aac8ff\">{s_cur_img_name_parent}<font>/<font color=\"#fc5a5a\">{cur_img_name.stem}<font><font color=\"#aac8ff\">{cur_img_name.suffix}<font>"
+            relative_path = f"{relative_path}/"
+
+        text_to_set = f"<font color=\"#aac8ff\">{self.dir_crawler.root_dir}/</font>" \
+                      f"<font color=\"#b6ff40\">{relative_path}</font>" \
+                      f"<font color=\"#fc5a5a\">{cur_img_name.stem}</font><font color=\"#aac8ff\">{cur_img_name.suffix}<font>"
         self.fnlbl.setText(text_to_set)
         self.pic_id.setText(f"Photo {position + 1}/{len(self.images)}")
 
@@ -385,6 +430,7 @@ class AppWindow(QWidget):
 
     def load_image(self, image):
         self.images.append(image)
+        self.rel_dir.add(Path(image.path.parent).relative_to(self.dir_crawler.root_dir))
         if len(self.images) == 1:
             self.to_image(0)
         self.pic_id.setText(f"Photo {self.current_image + 1}/{len(self.images)} (loading)")
@@ -392,6 +438,8 @@ class AppWindow(QWidget):
     def dir_crawler_finished(self):
         self.pic_id.setText(f"Photo {self.current_image + 1}/{len(self.images)}")
         self.picture_slider.setMaximum(len(self.images)-1)
+        self.completer.setModel(QStringListModel(self.rel_dir))
+        self.last_dirs_view.addItems(map(str, self.rel_dir))
 
     def init_crawling(self):
         self.dir_crawler = DirCrawler(self.select_dir())
@@ -413,7 +461,6 @@ class AppWindow(QWidget):
 def main():
     app = QApplication(sys.argv)
     app.setStyleSheet("QMainWindow{background-color: rgb(100,100,100)}")
-
     ex = AppWindow()
     sys.exit(app.exec_())
 
